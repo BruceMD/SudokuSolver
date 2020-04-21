2101e745b41475461a46f6f448175bd967771b64
BruceMD <maxbruce9@gmail.com>
2020-04-21 19:36:39 +0900

    First commit

---
+++ SudokuSolver.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+block = []
+grid = [[block for e in range(9)] for f in range(9)]
+
+
+
+
+mWindow = Tk()
+mWindow.geometry("1000x500")
+mWindow.title("Sudoku Solver v1.0")
+
+mWindow.mainloop()
